6d1595a61090e63fdb078f71afa022dc71c6d546
Konstantin Poleshchuk <javascriptoverlord@gmail.com>
2025-04-05 22:30:59 +0400

    new grab logic

--- scenes/player.gd
+++ scenes/player.gd
@@ -17,6 +17,7 @@ enum State {
 
 var current_state: State = State.WALK
 var near_ladder: bool = false
+var is_on_ladder: bool = false
 
 func _ready():
 	G.player = self
@@ -33,19 +34,15 @@ func _physics_process(delta):
 func process_walk_state(_delta):
 	_process_gravity(_delta)
 	_process_moving(_delta)
+	_process_ladder_moving(_delta)
 
-	if near_ladder:
-		_process_ladder_moving(_delta)
-
-	if !is_on_floor() and !near_ladder:
+	if !is_on_floor() and !is_on_ladder:
 		_change_state(State.IN_AIR)
 
 func process_in_air_state(_delta):
 	_process_gravity(_delta)
 	_process_moving(_delta)
-
-	if near_ladder:
-		_process_ladder_moving(_delta)
+	_process_ladder_moving(_delta)
 	
 	if is_on_floor():
 		_change_state(State.WALK)
@@ -54,9 +51,10 @@ func _process_gravity(_delta):
 	velocity.y += gravity * _delta
 
 func _process_moving(_delta):
-	var direction = Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * speed
+	var direction_x = Input.get_axis("move_left", "move_right")
+
+	if direction_x:
+		velocity.x = direction_x * speed
 		animation_player.play("walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
@@ -67,12 +65,19 @@ func _process_moving(_delta):
 	elif velocity.x < 0:
 		body.scale.x = -1
 
+	
 func _process_ladder_moving(_delta):
-	var direction = Input.get_axis("move_up", "move_down")
-	if direction:
-		velocity.y = direction * ladder_speed
-	else:
-		velocity.y = move_toward(velocity.y, 0, ladder_speed)
+	var direction_y = Input.get_axis("move_up", "move_down")
+
+	if !is_on_ladder and near_ladder and direction_y:
+		is_on_ladder = true
+		Logger.log_info(self.name, 'start to grab ladder')
+	
+	if is_on_ladder:
+		if direction_y:
+			velocity.y = direction_y * ladder_speed
+		else:
+			velocity.y = move_toward(velocity.y, 0, ladder_speed)
 
 func _change_state(new_state: State):
 	var old_state = current_state
@@ -99,6 +104,9 @@ func _on_ladder_detector_body_exited(_body: Node2D) -> void:
 func _on_ladder_left_timer_timeout() -> void:
 	Logger.log_info(self.name, 'ladder left timer timeout')
 	near_ladder = false
+	if is_on_ladder:
+		is_on_ladder = false
+		Logger.log_info(self.name, 'stop to grab ladder')
 
 func _death():
 	EB.player_death.emit()
