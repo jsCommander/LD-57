1dc2d16c11034be904b11d3343ca7865c003b89b
ValeryKhrennikov <gone.for.swing@gmail.com>
2025-04-06 21:00:27 +0400

    tunnels of entrances and exits to levels, hatches

--- scenes/splash_screen.gd
+++ scenes/splash_screen.gd
@@ -3,12 +3,12 @@ extends CanvasLayer
 @export var next_scene: G.GameScreens
 @onready var patient: Sprite2D = $patient
 @onready var player: Sprite2D = $player
-@onready var title: Sprite2D = $title
+
 
 func _ready() -> void:
 	Animations.shake(patient)
 	Animations.shake(player)
-	Animations.shake(title)
+
 
 func _handle_start_pressed() -> void:
 	Analytics.add_event("start pressed")
